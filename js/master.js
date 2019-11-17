f234b4ab0f7ca19d6b0a5e1c5219ba7348096012
h587729 <54106519+h587729@users.noreply.github.com>
2019-11-17 22:27:54 +0100

    Added Gitignore

--- js/master.js
+++ js/master.js
@@ -2,9 +2,9 @@ document.getElementById("hover").onmouseover = function() {mouseOver()};
 document.getElementById("hover").onmouseout = function() {mouseOut()};
 
 function mouseOver() {
-  document.getElementById("hover").style.color = "red";
+  document.getElementById("hover").style.color = "blue";
 }
 
 function mouseOut() {
-  document.getElementById("hover").style.color = "black";
+  document.getElementById("hover").style.color = "";
 }
